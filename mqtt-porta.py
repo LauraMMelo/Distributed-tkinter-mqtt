0af069c61d6b39b43cfb63064bc6e507317f7941
SauloMM <saulommelo96@gmail.com>
2018-11-08 17:32:54 -0300

    Final

---
+++ mqtt-porta.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 31 14:08:23 2018
+
+@author: computervision
+"""
+
+import paho.mqtt.client as paho
+import time
+
+broker = "test.mosquitto.org"
+
+
+client= paho.Client("client-004") #create client object client1.on_publish = on_publish #assign function to callback client1.connect(broker,port) #establish connection client1.publish("house/bulb1","on")
+
+
+
+print("connecting to broker ", broker)
+
+client.connect(broker)
+client.loop_start() #start loop to process received
+
+try:
+    while True:
+        client.publish("house/porta", str("Trancada"))#publish
+        time.sleep(2)
+
+except KeyboardInterrupt:
+    print("exiting")
+    client.disconnect
+    client.loop_stop()
--- mqtt-subscriber-lampada.py
+++ mqtt-subscriber-lampada.py
@@ -17,7 +17,7 @@ def on_message(client, userdata, message):
     time.sleep(.5)
     print("received message = ", str(message.payload.decode("utf-8")))
 
-client= paho.Client("client-002") #create client object client1.on_publish = on_publish #assign function to callback client1.connect(broker,port) #establish connection client1.publish("house/bulb1","on")
+client= paho.Client("client-005") #create client object client1.on_publish = on_publish #assign function to callback client1.connect(broker,port) #establish connection client1.publish("house/bulb1","on")
 
 #####bind function to callback
 client.on_message = on_message
